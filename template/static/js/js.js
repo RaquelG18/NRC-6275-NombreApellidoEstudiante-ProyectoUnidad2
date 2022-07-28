591915e33df2d8dd48e0dbfbb64775565beeef82
RaquelG18 <orgarces@espe.edu.ec>
2022-07-28 08:14:51 -0500

    update

--- template/static/js/js.js
+++ template/static/js/js.js
@@ -1,20 +1,40 @@
-  function allowDrop(ev) {
-    ev.preventDefault();
-  }
-
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
-
-var scoreText;
+// Creaci ́on de un puntaje 0 al iniciar el juego
 var score = 0;
-scoreText = game.add.text(5, 5, 'Points: 0', { font: '18px Arial', fill: '#0095DD' });
+// Creaci ́on de la variable etiqueta, para el score del puntaje
+var scoreLabel;
+
+function startGame() {
+  // Asignaci ́on de puntale en la etiqueta scorelabel()
+  scoreLabel = new createScoreLabel(10, 30);
+
+  }
+
+
+  /*
+Funci ́on para la etiqueta del puntaje o marcador del juego
+*/
+function createScoreLabel(x, y) {
+  this.score = 0;
+  this.x = x;
+  this.y = y;
+  }
+
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+
 
 
 
